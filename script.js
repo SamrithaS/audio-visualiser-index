5a51c83e28aa8b0d0c6706b0b41b5ee094aec766
SamrithaS <samritha@timeless.co>
2023-11-24 12:55:57 +0530

    transcription carousel

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
-// import fs from 'fs';
-// import fs from "fs/promises";
-let JSONValue;
 function main() {
-    let audio1 = new Audio();
-    audio1.src = "./audio1.mp3";
     const canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
     const ctx = canvas.getContext("2d");
+    let audio1 = new Audio();
     let analyser;
+    let JSONValue;
     let audioSource;
     let flag = 0;
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    audio1.src = "./audio1.mp3";
+
     query(audio1).then((response) => {
-        JSONValue = response
+        JSONValue = response;
+        JSONValue.forEach((element) => {
+            let newLine = document.createElement("p");
+            newLine.classList.add("audio-lines");
+            newLine.innerHTML = element.part;
+            document.getElementById("audio-text").append(newLine);
+        });
     });
+
+    showOrHideAudioText(audio1);
+
     document.getElementById("audio-svg").addEventListener("click", () => {
         const audioContext = new AudioContext();
-        if (!audio1.paused) audio1.pause();
-        else audio1.play();
+        if (!audio1.paused) {
+            audio1.pause();
+        } else audio1.play();
 
-        console.log(JSONValue, 'json value')
-
-        audio1.addEventListener('timeupdate', () => {
-            let currentTime = audio1.currentTime * 1000
-
-
-            JSONValue.forEach(element => {
-                if (currentTime >= element.start && currentTime <= element.end) {
-
-                    document.getElementById('audio-text').innerHTML = element.part
-                }
-            });
-
-        })
-
+        showOrHideAudioText(audio1);
+        audioTimeUpdateActions(audio1, JSONValue);
+        audioLineClickActions(audio1, JSONValue);
 
         if (flag === 0) {
             audioSource = audioContext.createMediaElementSource(audio1);
@@ -74,12 +72,59 @@ function main() {
 }
 main();
 
-async function query(audio1) {
+async function query() {
     let res;
-    // if (!audio1.paused) {
-    const fileName = await axios.get(`https://audio-visualizer-three.vercel.app/get`);
-
+    const fileName = await axios.get(
+        `https://audio-visualizer-three.vercel.app/get`
+    );
     res = fileName.data;
-    // }
-    return res
+    return res;
+}
+
+function showOrHideAudioText(audio) {
+    if (!audio.paused) {
+        document.getElementById("audio-text").classList.add("show");
+    } else {
+        document.getElementById("audio-text").classList.remove("show");
+    }
+}
+
+function audioTimeUpdateActions(audio, JSONValue) {
+    audio.addEventListener("timeupdate", () => {
+        let currentTime = audio.currentTime * 1000;
+
+        JSONValue.forEach((element, index) => {
+            if (currentTime >= element.start && currentTime <= element.end) {
+                document
+                    .getElementsByClassName("audio-lines")
+                [index].classList.add("selected-state");
+                document
+                    .getElementsByClassName("audio-lines")
+                [index].scrollIntoView({
+                    behavior: "smooth",
+                    block: "start",
+                    inline: "nearest",
+                });
+            } else {
+                document
+                    .getElementsByClassName("audio-lines")
+                [index].classList.remove("selected-state");
+            }
+        });
+    });
+}
+
+function audioLineClickActions(audio, JSONValue) {
+    for (
+        let i = 0;
+        i < document.getElementsByClassName("audio-lines").length;
+        i++
+    ) {
+        document
+            .getElementsByClassName("audio-lines")
+        [i].addEventListener("click", () => {
+            audio.currentTime = JSONValue[i].start / 1000;
+            audio.play();
+        });
+    }
 }
